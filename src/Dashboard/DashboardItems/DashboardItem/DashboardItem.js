0091da7500190a0c3d60e3386c26886bf765c51e
Michael Schultz <mikewschultz9029@gmail.com>
2022-10-15 10:42:08 -0400

    refactored icon img to be a background image - z-index was causing issues with the ellipsis being clickable

--- src/Dashboard/DashboardItems/DashboardItem/DashboardItem.js
+++ src/Dashboard/DashboardItems/DashboardItem/DashboardItem.js
@@ -4,8 +4,7 @@ export default function DashboardItem({ activityType, icon, currTime, prevTime }
 {
   return (
     // adds className determined by activityType - enables using props to determine styling, all that is needed to change or add style is add/change appropriate className in DashboardItem.css
-    <div className={"Dashboard-item Dashboard-panel rounded-corners " + activityType.toLowerCase().split(' ').join('-')}>
-      <img className="Dashboard-item-icon" src={icon} alt={activityType + 'icon'} />
+    <div className={"Dashboard-item Dashboard-panel rounded-corners " + activityType.toLowerCase().split(' ').join('-')} style={{ backgroundImage: `url(${icon})` }}>
       <div className="Dashboard-item-inner rounded-corners">
         <div className="Dashboard-item-top flex justify-space-between">
           <h3 className='Dashboard-item-title weight-hvy'>{activityType}</h3>
